b6aa4c3456c322f52b9f3ba33223d3711289c295
Sameer <sameer.prystech@gmail.com>
2024-07-03 00:49:34 +0500

    footer

--- theme/footer.php
+++ theme/footer.php
@@ -1,7 +1,25 @@
 <?php 
-
+    $copyright = get_field('footer' , 'options')["copyright"];
+    $links = get_field('footer' , 'options')["links"];
+    
+    // echo "<pre>";
+    //     var_dump($links);
+    // echo "</pre>";
 ?>
-  
+    <footer>
+        <section class="inner">
+            <div class="content">
+                <div class="copyright-and-name">
+                    <p><?php echo($copyright)?></p>
+                </div>
+                <div class="links">
+                    <?php foreach($links as $link){?>
+                        <a href="<?php echo($link["link"]["url"]) ?>"><?php echo($link["link"]["title"]) ?></a>
+                    <?php } ?>
+                </div>
+            </div>
+        </section>
+    </footer>
        
     
     </div><!-- closing all div -->
@@ -10,3 +28,8 @@
     <?php wp_footer(); ?>
 	</body>
 </html>
+
+    <!-- <?php foreach($links as $link){
+        var_dump($link["link"]["title"]);?>
+        <hr>
+    <?php } ?> -->
