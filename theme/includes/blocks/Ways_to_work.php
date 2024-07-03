0cfc92099f45d4ac289bc20f4e8ec5968d30de56
Ali Usman <ali.usmansajid02@gmail.com>
2024-07-04 00:51:31 +0500

    ways to work and footer

--- theme/includes/blocks/Ways_to_work.php
+++ theme/includes/blocks/Ways_to_work.php
@@ -1,9 +1,9 @@
 <?php 
     $heading = $block['heading'];
     $ContentBox = $block['content_box'];
-    $Content = $block["content_box"];
     $ScheduleText = $block['schedule_text'];
     $Button = $block['button'];
+
     // echo '<pre>';
     // var_dump($ContentBox);
     // echo '</pre>';
@@ -14,21 +14,33 @@
         <div class="inner">
             <h3 class="heading"><?php echo($heading) ?></h3>
             <div class="ways">
-                <div class="boxes-div">
+                <div class="boxes accordian">
                     <?php foreach($ContentBox as $box){?>
                         <div class="content-box">
-                            <h4 class="heading"><?php echo($box["box_heading"]) ?></h4>
-                            <?php foreach($box["content"] as $text){?>
-                                <p><?php echo($text["text"]) ?></p>    
-                            <?php } ?>
+                            <div class="heading-wrap">
+                                <h4 class="content-heading"><?php echo($box["box_heading"]) ?></h4>
+                            </div>
+                            <div class="content-wrap">
+                                <?php foreach($box["content"] as $text){?>
+                                    <p class="text"><?php echo($text["text"]) ?></p>    
+                                <?php } ?>
+                            </div>
                         </div>
                     <?php } ?>
-                </div>
-                <div class="book">
-                    <p> <?php echo($ScheduleText) ?> </p>
-                    <a href="<?php echo($Button["url"]) ?>"> <?php echo($Button["title"]) ?> </a>
-                </div>
-            </div>   
+                </div>  
+            </div>
+            <div class="schedule">
+                <p> <?php echo($ScheduleText) ?> </p>
+                <a href="<?php echo($Button["url"]) ?>"> <?php echo($Button["title"]) ?> </a>
+            </div>
         </div>
     </div>
 </section>
+
+<script>
+    $(document).ready(function() {
+        $('.content-box').click(function() {
+            $(this).toggleClass('active');
+        });
+    });
+</script>
