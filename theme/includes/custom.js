9b8871b9a6c7730807f2e9f8af0f0e7456fa401b
Anas <anashussain2711@gmail.com>
2024-07-05 03:53:50 +0500

    triangle

--- theme/includes/custom.js
+++ theme/includes/custom.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
-    console.log($('.slider'))
-
-    const swiper = new Swiper('.slider', {
-        slidesPerView: 3,          // Number of slides to show
-        spaceBetween: 70,          // Space between slides
-        slidesPerGroup: 1,         // Number of slides to scroll at a time
-        navigation: {              // Optional: enable navigation buttons
-            nextEl: '.slider-arrow.next',
-            prevEl: '.slider-arrow.prev',
-        },
-    });
-
-    console.log("swiper")
+    if ($(window).width() > 991){
+        const swiper = new Swiper('.slider', {
+            slidesPerView: 3,          // Number of slides to show
+            spaceBetween: 70,          // Space between slides
+            slidesPerGroup: 1,         // Number of slides to scroll at a time
+            navigation: {              // Optional: enable navigation buttons
+                nextEl: '.slider-arrow.next',
+                prevEl: '.slider-arrow.prev',
+            },
+        });
+    }
 })
