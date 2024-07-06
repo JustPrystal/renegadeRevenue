46b75a040dba0a8b533d3c85495a5759071bbc66
Qaim.Prystech <qaimprystech@gmail.com>
2024-07-06 05:14:35 +0500

    Contact form done

--- theme/includes/blocks/Banner.php
+++ theme/includes/blocks/Banner.php
@@ -6,18 +6,23 @@
 ?>
 
 <div class="banner">
-    <img class="background" src=<?php echo $backgroundImage ?> alt="img">
-    <div class="container">
-        <div class="content-wrap">
-            <h2 class="heading">
-                <?php echo $heading; ?>
-            </h2>
-            <p class="description">
-                <?php echo $description; ?>
-            </p>
-            <div class="button-wrap">
-                <a href=<?php echo $link['url'] ?> class="button"><?php echo $link['title'] ?></a>
+    <div class="inner">
+        <img class="background" src=<?php echo $backgroundImage ?> alt="img">
+        <div class="container">
+            <div class="content-wrap">
+                <h2 class="heading" data-aos="fade-right">
+                    <?php echo $heading; ?>
+                </h2>
+                <p class="description">
+                    <?php echo $description; ?>
+                </p>
+                <div class="button-wrap">
+                    <a href=<?php echo $link['url'] ?> class="button"><?php echo $link['title'] ?></a>
+                </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+  AOS.init();
+</script>
--- theme/includes/blocks/contact.php
+++ theme/includes/blocks/contact.php
@@ -1,1 +1,31 @@
-<?php echo do_shortcode($block['shortcode']) ?>
+<section class="contact">
+    <div class="container">
+        <div class="top-wrap">
+            <h2 class="heading">
+                <?php echo $block['heading'] ?>
+            </h2>
+            <p class="description">
+                <?php echo $block['description'] ?>
+            </p>
+        </div>
+        <div class="bio-form-wrap">
+            <div class="bio-wrap">
+                <p class="name text">
+                    <?php echo $block['name'] ?>
+                </p>
+                <p class="company-name text">
+                    <?php echo $block['company_name'] ?>
+                </p>
+                <p class="contact-number text">
+                    <?php echo $block['contact_number'] ?>
+                </p>
+                <p class="email text">
+                    <?php echo $block['email'] ?>
+                </p>
+            </div>
+            <div class="form-wrap">
+                <?php echo do_shortcode($block['shortcode']) ?> 
+            </div>
+        </div>
+    </div>
+</section>
