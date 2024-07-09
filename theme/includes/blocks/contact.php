7c7684eb876c2fe96f472ef8f73eb52eff6cf867
Qaim.Prystech <qaimprystech@gmail.com>
2024-07-09 06:24:42 +0500

    Contact update.

--- theme/includes/blocks/contact.php
+++ theme/includes/blocks/contact.php
@@ -8,24 +8,8 @@
                 <?php echo $block['description'] ?>
             </p>
         </div>
-        <div class="bio-form-wrap">
-            <div class="bio-wrap">
-                <p class="name text">
-                    <?php echo $block['name'] ?>
-                </p>
-                <p class="company-name text">
-                    <?php echo $block['company_name'] ?>
-                </p>
-                <p class="contact-number text">
-                    <?php echo $block['contact_number'] ?>
-                </p>
-                <p class="email text">
-                    <?php echo $block['email'] ?>
-                </p>
-            </div>
-            <div class="form-wrap">
-                <?php echo do_shortcode($block['shortcode']) ?> 
-            </div>
+        <div class="form-wrap">
+            <?php echo do_shortcode($block['shortcode']) ?> 
         </div>
     </div>
 </section>
