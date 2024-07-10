378be4bef9f63f27126724fcea40e741375598a0
Anas <anashussain2711@gmail.com>
2024-07-11 00:27:38 +0500

    kuch

--- theme/includes/custom-functions.php
+++ theme/includes/custom-functions.php
@@ -58,4 +58,48 @@
         wp_send_json_success($output, 200);
     }
 
+
+    function cptui_register_my_cpts_article() {
+
+        /**
+         * Post Type: Articles.
+         */
+    
+        $labels = [
+            "name" => esc_html__( "Articles", "custom-post-type-ui" ),
+            "singular_name" => esc_html__( "Article", "custom-post-type-ui" ),
+        ];
+    
+        $args = [
+            "label" => esc_html__( "Articles", "custom-post-type-ui" ),
+            "labels" => $labels,
+            "description" => "",
+            "public" => true,
+            "publicly_queryable" => true,
+            "show_ui" => true,
+            "show_in_rest" => true,
+            "rest_base" => "",
+            "rest_controller_class" => "WP_REST_Posts_Controller",
+            "rest_namespace" => "wp/v2",
+            "has_archive" => "articles",
+            "show_in_menu" => true,
+            "show_in_nav_menus" => true,
+            "delete_with_user" => false,
+            "exclude_from_search" => false,
+            "capability_type" => "post",
+            "map_meta_cap" => true,
+            "hierarchical" => true,
+            "can_export" => false,
+            "rewrite" => [ "slug" => "article", "with_front" => true ],
+            "query_var" => true,
+            "supports" => [ "title", "editor", "thumbnail", "excerpt", "custom-fields", "page-attributes" ],
+            "taxonomies" => [ "category" ],
+            "show_in_graphql" => false,
+        ];
+    
+        register_post_type( "article", $args );
+    }
+    
+    add_action( 'init', 'cptui_register_my_cpts_article' );
+    
 ?>
