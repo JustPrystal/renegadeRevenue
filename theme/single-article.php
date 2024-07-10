e25204ace48cad977ba3f52e823b5def807361a9
Qaim.Prystech <qaimprystech@gmail.com>
2024-07-10 05:00:44 +0500

    Single-article added

--- theme/single-article.php
+++ theme/single-article.php
@@ -1,1 +1,32 @@
-asdfghjkl
+<?php 
+    get_header();
+    global $post;
+?>
+
+<section class="single-article">
+    <div class="container">
+        <h2 class="heading">
+            <?php echo $post->post_title?>
+        </h2>
+        <div class="image"><?php the_post_thumbnail(); ?></div>
+        <div class="author-info">
+            <div class="author-image">
+                
+            </div>
+            <div class="text-wrap">
+                <div class="author-name">
+                    <?php echo $post->post_author ?>
+                </div>
+                <div class="creation-date">
+                    <?php echo $post->post_date?> 
+                </div>
+            </div>
+
+        </div>
+        <div class="decription">
+            <?php echo $post->post_content?>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
